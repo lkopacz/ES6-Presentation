efc2a04dfafc306e5a77349263f13242fb45d932
Lindsey Kopacz <lindsey.kopacz@acquia.com>
2018-01-18 11:02:27 -0500

    Started sample file.

---
+++ samples.js
@@ -0,0 +1,29 @@
+// var vs let vs const 
+// Talk about hoisting and examples of each
+// Talk about how const is different from other languages such as PHP
+// const being a reference vs something you cannot change. 
+
+
+// Arrow Function
+// Post here what it would be in old school function
+
+const person = {
+  age: 28,
+  name: 'Lindsey',
+
+  increaseAge: (number) => {
+    this.age = this.age + number,
+  },
+};
+
+// We should also provide an example of how arrow function can be definied in a constant. 
+
+
+
+// Anticipated Question
+// Q1) Why would you write it that way?
+// A1) Benefits: I don't have to define a const. You can also write a function anonymously
+// A1) Differentiate how scoping is being impacted by using arrow function vs function 
+
+//Intro into classes
+
